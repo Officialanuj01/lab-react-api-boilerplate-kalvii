2e3af96239808cefe89ee90895e0e8b21b9b7078
Officialanuj01 <142708397+Officialanuj01@users.noreply.github.com>
2024-01-15 22:29:45 +0530

    done
    it changes
    Commit summary
    Add files via upload
    Optional extended description

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './App.css'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+
+
+const App = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      try {
+        const response = await axios.get("https://reactnd-books-api.udacity.com/books",{ headers: { 'Authorization': 'whatever-you-want' }});
+        setBooks(response.data.books);
+      } catch (error) {
+        console.error("Error Encountered : ", error.message);
+      }
+    };
+    getBooks()
+  }, []);
+
+  return (
+    <>
+    <div>
+      <ul>
+        {books.map((book) => (
+          <div className="container">
+            <div className="box">
+            <h1>{book.title}</h1>
+              <div className='img'>
+                <img src={book.imageLinks.thumbnail} alt="" />
+              </div>
+              <div className='text'>
+                <p>{book.description}</p>
+              </div>
+              <div className="author">
+                <p>{book.authors.join(",")}</p>
+              </div>
+            </div>
+          </div>
+          
+        ))}
+      </ul>
+    </div>
+    </>
+  )
+}
+
+export default App;
